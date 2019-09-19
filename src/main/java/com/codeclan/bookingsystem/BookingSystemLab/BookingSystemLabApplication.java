3737eb799e17be327c7fd7b32c7321721bf68ee7
DANIELA <danielamiritescu@gmail.com>
2019-09-19 10:30:02 +0100

    initial commit

---
+++ src/main/java/com/codeclan/bookingsystem/BookingSystemLab/BookingSystemLabApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.bookingsystem.BookingSystemLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemLabApplication.class, args);
+	}
+
+}
